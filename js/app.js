8e36dbe90f85cc87b2605d6d6d7266e4cf184ff3
JulissaJ <julissa.jansen@gmail.com>
2014-02-28 14:57:08 -0500

    First commit of guess the number game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-alert('Hey find me to know where to put the javascript');
+// alert('Hey find me to know where to put the javascript');
+
+var num = Math.floor((Math.random()*100)+1);
+
+
+var name = prompt("What is your name?");
+
+
+var guess = prompt("Guess a number between 1 and 100");
+
+
+
+if (guess == num) {
+  alert('Congratulations '+ name +', you\'re correct!');
+}
+
+else { (guess != num)
+  alert('Wrong ' + name + '. You\'ve lost!');
+}
+
+// else {
+//   (guess !=== num) {
+//   alert('Wrong number!');
+// }
+
